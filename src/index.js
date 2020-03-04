61471632f72315521884ce500d07c8803e768873
pavel <pavel.pronchatov155@gmail.com>
2020-03-04 23:35:00 +0400

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = str.split("");
+
+  for(let i = 0; i < stack.length; i++){
+    for(let j = 0; j < bracketsConfig.length; j++){
+      if(bracketsConfig[j][0] === stack[i] && bracketsConfig[j][1] ===stack[i + 1]){
+        stack.splice(i,2);
+        i = -1;
+        console.log(stack);
+      }
+    }
+  }
+
+  console.log(stack);
+  if(stack.length == 0) {
+    return true;
+  }else{
+    return false;
+  }
+
 }
+
